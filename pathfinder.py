4f0f9baa4194714c22974fc38c844a3292b6cb55
Lisa Williams <lisaismyname0@gmail.com>
2022-06-18 11:57:44 -0400

    started playing with pillow image editing

--- pathfinder.py
+++ pathfinder.py
@@ -1,3 +1,7 @@
 with open("elevation_small.txt") as elevation_text:
     elevation_grid = elevation_text.read()
-    print(type(elevation_grid))
+    print(elevation_grid)
+
+from PIL import Image
+cat_pic = Image.open("cat.png")
+print(cat_pic)
