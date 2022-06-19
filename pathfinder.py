8a6315283a8f6c10d42da6c13420e72c5c855acb
Lisa Williams <lisaismyname0@gmail.com>
2022-06-19 11:25:11 -0400

    can log min and max values in array

--- pathfinder.py
+++ pathfinder.py
@@ -1,7 +1,11 @@
 with open("elevation_small.txt") as elevation_text:
-    elevation_grid = elevation_text.read()
-    print(elevation_grid)
+    elevation_grid = elevation_text.readlines()
+    split0 = elevation_grid[0].split()
+    min_elevation = min(split0)
+    print(min_elevation)
+    max_elevation = max(split0)
+    print(max_elevation)
+    while min_elevation <= max_elevation:
+        
 
-from PIL import Image
-cat_pic = Image.open("cat.png")
-print(cat_pic)
+    list0_dict = dict.fromkeys(split0, 0)
